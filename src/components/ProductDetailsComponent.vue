792f9bbcfdf0b083fbc13554cc8c29e3abc0ea11
Soboleva Olga <osoboleva27@gmail.com>
2024-03-05 04:04:53 +0300

    initial

---
+++ src/components/ProductDetailsComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3>Название продукта:{{ this.product.name }}</h3>
+        <p>Цена продукта:{{ formattedPrice}}</p>
+        <p v-if="product.available">Доступность товара: Available</p>
+        <p v-else>Доступность товара: Out of stock</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                product:{
+                    name:'Гель для душа',
+                    price:99.99,
+                    available:true,
+                },
+            };
+        },
+        computed:{
+            formattedPrice(){
+                return '$'+this.product.price;
+            },
+        },    
+            
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
